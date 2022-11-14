f74a69b1d3ed6f4d3c1e5afaa13b3e931a1c57da
Dirk Munro <dirkmunro8@gmail.com>
2022-11-14 10:05:00 +0100

    changed logging in run_all_cases so that sall timing differences is not picked up by git

--- run_all_prb.sh
+++ run_all_prb.sh
@@ -2,9 +2,12 @@
 grep 'if' prbs.py | grep -o -P "(?<=').*(?=')" > prb_lst.log
 #
 rm prb_err.log 2> /dev/null
+#
 while read p; do
     echo "Problem: $p"
+    echo "Running problem $p. The run is error free if no further output follows." >> prb_err.log
     python main.py $p 2>> prb_err.log
-    cp history.log ./logs/"P${p}_hist.log"
+    cat history.log  | cut -c 1-71 > ./logs/"p${p}_hist.log"
+#
 done < prb_lst.log
 #
