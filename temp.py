9c1a0cd2067fa2eefcc896c3867297f31ee73a59
Dirk Munro <dirkmunro8@gmail.com>
2022-12-23 04:30:02 +0100

    just temp

--- temp.py
+++ temp.py
@@ -1,114 +1,104 @@
 #
 import numpy as np
-from prob.util.topo2d_ei import topo2d_init
-from prob.util.topo2d_ei import topo2d_simu
-#
-# specify subsolver here
-#
-from subs.t2dual import t2d as subs
-#
-# specify problem and algorithmic parameters here
-#
-def apar(n):
-#   
-    mov=2e-1*np.ones(n,dtype=float)
-    asf=[0.5,1.5]
-#
-    enf='none'
-#
-    kmx=1000
-    cnv=[1e-1,1e-1,1e-3,1e-3,1e-3]
-#
-    return mov, asf, enf, kmx, cnv
+from prob.util.topo2d_neg import topo2d_init
+from prob.util.topo2d_neg import topo2d_simu
+from subs.t2duel import t2d as subs
 #
 def caml(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
-    c_x=2e0*np.absolute(df_k)/np.maximum(x_k,1e-6)
-    c_x[1:]=0e0
+    c_x=np.ones_like(df_k)
+    if k > 0:
+        sph = 2.*(f_1 - f_k - np.dot(df_k,(x_1-x_k)))/np.maximum(np.linalg.norm(x_1-x_k)**2.,1e-6)
+        for j in range(len(f_k)): c_x[j]=sph[j]
 #
     c_x[0]=np.maximum(c_x[0],1e-6)
 #
-    if k>20000:
-        mov=np.where((x_k-x_1)*(x_1-x_2) <= 0., mov*asf[0], mov*asf[1])
-        mov=np.minimum(np.maximum(mov,1e-3),0.2)
+    L=x_k
+    U=x_k
 #
-    L=L_k
-    U=U_k
-#
-    d_l = np.maximum(x_k-mov*(x_u-x_l),x_l)
-    d_u = np.minimum(x_k+mov*(x_u-x_l),x_u)
+    d_l = np.maximum(x_k - mov*(x_u-x_l),x_l)
+    d_u = np.minimum(x_k + mov*(x_u-x_l),x_u)
 #
     return c_x,mov,L,U,d_l,d_u
 #
+def apar(n):
+#
+    mov=0.1*np.ones(n)
+    asf=[0.7,1.1]
+#
+    enf='none'
+#
+    kmx=10000
+    cnv=[1e-6,1e-6,1e-6,1e-6,1e-6]
+#
+    return mov, asf, enf, kmx, cnv
+#
 def init(g):
 #
-    mm=3
-    nelx=80*mm
-    nely=10*mm
-    v_l = 0.1
-    v_0 = 0.4
-    v_u = 0.4
+    nelx = 180
+    nely = 60
+    v_l = 0.0
+    v_0 = 1.0
+    v_u = 0.0
 #
     ft = 1
-    rmin = 2.
-    dext=0#int(np.ceil(rmin))
-    felx = nelx+dext
-    fely = nely+2*dext
+    rmin = 5.4
+    felx = 180
+    fely = 60
+    xPadd = -1*np.ones(felx*fely,dtype=float)
 #
-    xPadd=np.zeros((felx,fely),dtype=float)
-    tmp=-1*np.ones((nelx,nely),dtype=float)#.reshape(nelx,nely)
-    xPadd[:nelx,dext:fely-dext]=tmp
-    xPadd[nelx-dext:,nely+dext:]=1.
-    xPadd[nelx:,nely:nely+dext]=1.
-    xPadd=xPadd.flatten()
-#
+#   tmp=x.reshape(nelx,nely)
+#   x_ext=np.zeros((nelx_ext,nely_ext))
+#   x_ext[:nelx,d_ext:nely_ext-d_ext]=tmp
+#   x_ext[nelx-d_ext:,nely+d_ext:]=1.
+#   x_ext[nelx:,nely:nely+d_ext]=1.
+#   x_ext=x_ext.flatten()
+# 
     # BC's and support
-    ndof=2*(nelx+1)*(nely+1)
     dofs=np.arange(2*(nelx+1)*(nely+1))
+    fix=np.union1d(dofs[0:2*(nely+1):2],np.array([2*(nelx+1)*(nely+1)-1]))
+
+    # Set load
+    frc=[(1,-1)]
 #
-    dely=2*(nely+1)
-    left = np.union1d(dofs[0:dely:2], dofs[1:dely:2])
-    right = np.union1d(dofs[ndof - dely:ndof:2],\
-        dofs[ndof - dely + 1:ndof:2])
-    fix = np.union1d(left, right)
-#
-#   fix=np.union1d(dofs[0:4:1], dofs[2*(nely+1)-1:2*(nely+1)-5:-1])
-#
-    pen = 3.0
-    qen = 1.0
-    muc = 1e-2
-    Emin = 1e-9; Emax=1.0
-    gv = 0.0/nelx/nely
+    pen =  4.0
+    qen =  1.0
+    muc =  0.0
+    Emin=1e-6; Emax=1.0
+    gv=0e0#-9.81#/nelx/nely
 #
     n = nelx*nely
     m = 1
-    x_l = np.zeros(n,dtype=float)
+    x_l = -1*np.ones(n,dtype=float)
     x_u = np.ones(n,dtype=float)
     x_k = v_0*np.ones(n,dtype=float)
 #
-    aux=topo2d_init(nelx,nely,v_l,v_0,v_u,ft,rmin,felx,fely,xPadd,fix,pen,qen,muc,Emin,Emax,gv,g)
+    aux=topo2d_init(nelx,nely,v_l,v_0,v_u,ft,rmin,felx,fely,xPadd,fix,frc,pen,qen,muc,Emin,Emax,gv,g)
 #
     c_s=np.ones(m)
+    c_s[0]=0
 #
     return n,m,x_l,x_u,x_k,c_s,aux
 #
 def simu(n,m,x,aux,g):
-#
-    v_l=aux[2]
-    v_u=aux[4]
 #
     f = np.zeros((m + 1), dtype=float)
     df = np.zeros((m + 1, n), dtype=float)
 #
     [c,dc,v,dv]=topo2d_simu(n,m,x,aux,g)
 #
-    f[0]=c#/2600#0
-    f[1]=v/n/v_u-1.
-#   f[2]=-v/n/v_l+1.
+    v_l=aux[2]
+    v_u=aux[4]
 #
-    df[0][:] = dc#/2600#0
-    df[1][:] = dv/n/v_u
-#   df[2][:] = -dv/n/v_l
+    pen=aux[10]
+#
+    f[0]=c
+    f[1]=v/n-v_u
+#   f[2]=v_l-v/n
+#
+    df[0][:] = dc
+    df[1][:] = dv/n
+#   df[2][:] = -dv/n
 #
     return f, df
 #
