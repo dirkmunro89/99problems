1ccdd51084015fbf575cc90ee62b5ba3d1e32dca
Dirk Munro <dirkmunro8@gmail.com>
2022-12-24 10:03:32 +0100

    small additiona

--- cmls.py
+++ cmls.py
@@ -1,10 +1,25 @@
 #
 import numpy as np
 #
+def t2al(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
+#
+    c_x=np.ones_like(df_k)*1e-6
+    c_x[0]=2e0*np.absolute(df_k[0])/np.maximum(x_k,1e-6)
+#
+    c_x[0]=np.maximum(c_x[0],1e-6)
+#
+    L=L_k
+    U=U_k
+#
+    d_l = np.maximum(x_k-mov*(x_u-x_l),x_l)
+    d_u = np.minimum(x_k+mov*(x_u-x_l),x_u)
+#
+    return c_x,mov,L,U,d_l,d_u
+#
 def t2rl(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
     c_x=np.ones_like(df_k)*1e-6
-    c_x[0]=-2e0*(df_k[0])/(x_k+1e-3)
+    c_x[0]=-2e0*(df_k[0])/np.maximum(x_k,1e-6)
 #
     c_x[0]=np.maximum(c_x[0],1e-6)
 #
