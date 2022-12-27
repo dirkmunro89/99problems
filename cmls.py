9ff7d5d1e3c229c788ad0c56c2bd03fc51cb533b
Dirk Munro <dirkmunro8@gmail.com>
2022-12-27 18:00:46 +0100

    updated

--- cmls.py
+++ cmls.py
@@ -3,7 +3,7 @@ import numpy as np
 #
 def t2al(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
-    c_x=np.ones_like(df_k)*1e-6
+    c_x=np.zeros_like(df_k)
     c_x[0]=2e0*np.absolute(df_k[0])/np.maximum(x_k,1e-6)
 #
     c_x[0]=np.maximum(c_x[0],1e-6)
@@ -18,7 +18,7 @@ def t2al(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
 def t2rl(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
-    c_x=np.ones_like(df_k)*1e-6
+    c_x=np.zeros_like(df_k)
     c_x[0]=-2e0*(df_k[0])/np.maximum(x_k,1e-6)
 #
     c_x[0]=np.maximum(c_x[0],1e-6)
@@ -33,7 +33,7 @@ def t2rl(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
 def t2sl(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
-    c_x=np.ones_like(df_k)*1e-6
+    c_x=np.zeros_like(df_k)
     if k > 0:
         sph = 2.*(f_1 - f_k - np.dot(df_k,(x_1-x_k)))/np.maximum(np.linalg.norm(x_1-x_k)**2.,1e-6)
         c_x[0]=sph[0]
@@ -52,7 +52,7 @@ def t2sl(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
 def t2ml(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
-    c_x=np.ones_like(df_k)*1e-6
+    c_x=np.zeros_like(df_k)
 #
     if k<=1:
         L = x_k-0.5*(x_u-x_l)
@@ -82,7 +82,8 @@ def t2ml(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
 def mml(k, x_k, f_k, df_k, f_1, x_1, x_2, L_k, U_k, x_l, x_u, asf, mov):
 #
-    c_x=np.ones_like(df_k)*1e-6
+    c_x=np.zeros_like(df_k)
+    c_x[0] = np.maximum(c_x[0],1e-6)
 #
     if k>2:
         mov=np.where((x_k-x_1)*(x_1-x_2) <= 0., mov*asf[0], mov*asf[1])
--- run_sws_prb.sh
+++ run_sws_prb.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-nams="3a 3b 3c 3d 3e 3l 3f 3g 3i 3jj 3kk"
+#nams="a b c d e f g h i j k l m n o p q"
+#for n in $nams; do
+#    name="Ntop_swei1_"$n
+#    python main.py $name
+#    gnuplot -e "xi=120;yi=60" topomake.p
+#    mv topology.eps ./self/${name}_topo.eps
+#    mv history.log ./self/${name}_hist.log
+#done
+#nams="a b c d e f g h i j k l m n o p q"
+#for n in $nams; do
+#    name="Ntop_swei2_"$n
+#    python main.py $name
+#    gnuplot -e "xi=120;yi=60" topomake.p
+#    mv topology.eps ./self/${name}_topo.eps
+#    mv history.log ./self/${name}_hist.log
+#done
+nams="a b c d e f g h i j k l m n o p q"
 for n in $nams; do
-    name="Ntop_swei_enf"$n
-    python main.py $name
-    gnuplot -e "xi=120;yi=60" topomake.p
-    mv topology.eps ./eelf/${name}_topo.eps
-    mv history.log ./eelf/${name}_hist.log
-done
-nams="1a 1b 1c 1d 1e 1l 1f 1g 1i 1jj 1kk"
-for n in $nams; do
-    name="Ntop_swei_enf"$n
-    python main.py $name
-    gnuplot -e "xi=120;yi=60" topomake.p
-    mv topology.eps ./eelf/${name}_topo.eps
-    mv history.log ./eelf/${name}_hist.log
-done
-nams="2a 2b 2c 2d 2e 2l 2f 2g 2i 2jj 2kk"
-for n in $nams; do
-    name="Ntop_swei_enf"$n
-    python main.py $name
-    gnuplot -e "xi=240;yi=60" topomake.p
-    mv topology.eps ./eelf/${name}_topo.eps
-    mv history.log ./eelf/${name}_hist.log
-done
-nams="4a 4b 4c 4d 4e 4l 4f 4g 4i 4jj 4kk"
-for n in $nams; do
-    name="Ntop_swei_enf"$n
+    name="Ntop_swei3_"$n
     python main.py $name
     gnuplot -e "xi=240;yi=60" topomake.p
-    mv topology.eps ./eelf/${name}_topo.eps
-    mv history.log ./eelf/${name}_hist.log
+    mv topology.eps ./self/${name}_topo.eps
+    mv history.log ./self/${name}_hist.log
+done
+nams="a b c d e f g h i j k l m n o p q"
+for n in $nams; do
+    name="Ntop_swei4_"$n
+    python main.py $name
+    gnuplot -e "xi=240;yi=60" topomake.p
+    mv topology.eps ./self/${name}_topo.eps
+    mv history.log ./self/${name}_hist.log
 done
