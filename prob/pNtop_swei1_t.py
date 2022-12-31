89d64a102fdbb37efee82ffc2d595931a2a03b2a
Dirk Munro <dirkmunro8@gmail.com>
2022-12-31 09:11:29 +0100

    added ML curv cases...

---
+++ prob/pNtop_swei1_t.py
@@ -0,0 +1,20 @@
+#
+import numpy as np
+#
+from prob.grnd.pNtop_swei1 import init
+from prob.grnd.pNtop_swei1 import simu
+from cmls import t2dl as caml
+from subs.t2dual import t2d as subs
+#
+def apar(n):
+#   
+    mov=1e-1*np.ones(n,dtype=float)
+    asf=[0.7,1.2]
+#
+    enf='t-r'
+#
+    kmx=5000
+    cnv=[1e-3,1e0,1e0,1e6,1e0]
+#
+    return mov, asf, enf, kmx, cnv
+#
