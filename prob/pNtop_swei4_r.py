4d57fa20601a2ab3c4401491619ce0dc325e8fe1
Dirk Munro <dirkmunro8@gmail.com>
2022-12-31 08:27:22 +0100

    to level with DB

---
+++ prob/pNtop_swei4_r.py
@@ -0,0 +1,20 @@
+#
+import numpy as np
+#
+from prob.grnd.pNtop_swei4 import init
+from prob.grnd.pNtop_swei4 import simu
+from cmls import t2el as caml
+from subs.t2dual import t2d as subs
+#
+def apar(n):
+#   
+    mov=1e-1*np.ones(n,dtype=float)
+    asf=[0.7,1.2]
+#
+    enf='none'
+#
+    kmx=5000
+    cnv=[1e-3,1e0,1e0,1e6,1e0]
+#
+    return mov, asf, enf, kmx, cnv
+#
